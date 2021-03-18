0b682099888c8186bd1cd83c4d19afa65ef8dfa1
nawsusunyein99 <T951226@wavemoney.com.mm>
2021-03-18 16:23:06 +0630

    commit test from person1

--- BasicMLCatDogClassifier/BasicMLCatDogClassifier/ViewController.swift
+++ BasicMLCatDogClassifier/BasicMLCatDogClassifier/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("this is my test from person 1.")
     }
 
     @IBAction func checkAnimalAction(_ sender: Any) {
