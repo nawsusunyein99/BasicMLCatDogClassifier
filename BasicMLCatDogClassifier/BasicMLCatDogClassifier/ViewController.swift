3e998c708c514926f1f2f6a9386f3eed800e38b2
nawsusunyein99 <T951226@wavemoney.com.mm>
2021-03-18 16:21:16 +0630

    test

--- BasicMLCatDogClassifier/BasicMLCatDogClassifier/ViewController.swift
+++ BasicMLCatDogClassifier/BasicMLCatDogClassifier/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var checkAnimalButton : UIButton!
+    @IBOutlet weak var classificationLabel : UILabel!
+    @IBOutlet weak var imageView : UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func checkAnimalAction(_ sender: Any) {
+    }
+    
+    
+    
+    @IBAction func selectImageSource(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        
+        let imageSourceActions = UIAlertController(title: "Image Source", message: "Choose an image source to continue add", preferredStyle: .actionSheet)
+        imageSourceActions.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: {(action : UIAlertAction) in
+            imagePicker.sourceType = .photoLibrary
+            self.present(imagePicker, animated: true)
+        }))
+        imageSourceActions.addAction(UIAlertAction(title: "Cancel",style: .cancel))
+    }
 }
 
+extension UIViewController : UIImagePickerControllerDelegate,UINavigationControllerDelegate{
+    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+         
+    }
+    
+    public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true)
+    }
+}
